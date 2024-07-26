db6c04a78f9cc6903d347221d873da527c24cd4b
Arash Tajdar <arashtajdar@gmail.com>
2024-07-26 13:44:53 +0200

    Update server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,3 @@
 module.exports = ({ env }) => ({
-  url: env('RAILWAY_PUBLIC_DOMAIN', 'strapi-rn6j-develop.up.railway.app')
+  url: 'https://' + env('RAILWAY_PUBLIC_DOMAIN', 'strapi-rn6j-develop.up.railway.app/')
 });
