f6bf8f2ff9548155e1a2979ccb104b23171f0805
Arash Tajdar <arashtajdar@gmail.com>
2024-07-26 12:47:04 +0200

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,12 +2,11 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('DATABASE_HOST', process.env.DATABASE_HOST),
+            host: env('DATABASE_HOST', 'monorail.proxy.rlwy.net),
             port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', process.env.DB_NAME),
-            user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
-            password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
-            ssl: env.bool('DATABASE_SSL', false),
+            database: env('DATABASE_NAME', 'railway),
+            user: env('DATABASE_USERNAME', process.env.POSTGRES_USER),
+            password: env('DATABASE_PASSWORD', process.env.POSTGRES_PASSWORD),
         },
         useNullAsDefault: true,
     },
