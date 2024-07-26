05df6d5a5038dc436b4b0bfff667c55eab1c9c43
Arash Tajdar <arashtajdar@gmail.com>
2024-07-26 12:54:07 +0200

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,12 +2,20 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('DATABASE_HOST', 'monorail.proxy.rlwy.net),
-            port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'railway),
-            user: env('DATABASE_USERNAME', process.env.POSTGRES_USER),
-            password: env('DATABASE_PASSWORD', process.env.POSTGRES_PASSWORD),
+            host: 'monorail.proxy.rlwy.net,
+            port:  5432,
+            database: 'railway',
+            user: env('POSTGRES_USER'),
+            password: env('POSTGRES_PASSWORD'),
         },
         useNullAsDefault: true,
     },
 });
+
+// module.exports = ({ env }) => ({
+//     connection: {
+//         connection: {
+//             connectionString: env('DATABASE_URL')
+//         }
+//     }
+// });
