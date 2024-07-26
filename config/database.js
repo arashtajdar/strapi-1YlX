91fcf2c5cf7c20646c5c375b3248f84700eb524d
Arash Tajdar <arashtajdar@gmail.com>
2024-07-26 11:45:11 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,20 @@
-module.exports = ({ env }) => ({
-    connection: {
-        client: 'postgres',
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+    
+    return {
         connection: {
-            connectionString: env('DATABASE_PRIVATE_URL')
+            client: 'postgres',
+            connection: {
+                host,
+                port,
+                database,
+                user,
+                password,
+                ssl: { rejectUnauthorized: false },
+            },
+            debug: false,
         },
-        pool: { min: 0 }
     }
-});
+};
