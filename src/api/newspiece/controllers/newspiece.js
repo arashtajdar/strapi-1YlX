34e9ffca5cd6e2a6cfe8e656cf738dcdf0e1661d
a.tajdar@united-remote.com <a.tajdar@united-remote.com>
2024-09-11 21:04:31 +0200

    Update

---
+++ src/api/newspiece/controllers/newspiece.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newspiece controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::newspiece.newspiece');
